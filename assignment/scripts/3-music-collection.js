378b05dc188c0e5c3c98ebb9c55e3d4817fdcebe
Josh Snyder <joshua.t.snyder@gmail.com>
2022-07-10 11:15:12 -0500

    console logged the array and added coding notes

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -18,3 +18,8 @@ addToCollection ( 'Whispers', 'Passenger', '2014');
 addToCollection ( 'Onwards and Sidways', 'Joshua Radin', '2015');
 addToCollection ( 'Blurryface', 'Twenty One Pilots', '2015');
 addToCollection ( 'Leavetaking', 'Peter Bradley Adams', '2008');
+
+console.log(collection); //Test the addToCollection function:
+    //Add 6 albums to your collection. Aim to have a mix of both same and different artists and published years. (Feel free to share your musical interests, or make stuff up. Totally fine either way.)
+    //Console.log each album as added using the returned value.
+    //After all are added, console.log the collection array.
