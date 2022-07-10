66dfe1fa1a4af8332ff6b0b2cb37b98f7dd6a19e
Josh Snyder <joshua.t.snyder@gmail.com>
2022-07-10 10:37:43 -0500

    added object to function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,5 +3,12 @@ console.log('***** Music Collection *****')
 let collection = []
 
 function addToCollection (title, artist, yearPublished) {
-    
-}
+    const album = {title, artist, yearPublished};
+    collection.push (album);
+    return console.log(album);
+}   //Add a function named addToCollection. This function should:
+    //Take in the album's title, artist, yearPublished as input parameters
+    //Create a new object having the above properties
+    //Add the new object to the end of the collection array
+    //Return the newly created object
+
