030ef09667adba2d1b4d0d79e7f232ab328f07d7
Josh Snyder <joshua.t.snyder@gmail.com>
2022-07-10 11:52:42 -0500

    added function to show collection of albums

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,7 +3,7 @@ console.log('***** Music Collection *****')
 let collection = []
 
 function addToCollection (title, artist, yearPublished) {
-    const album = {title, artist, yearPublished};
+    let album = {title, artist, yearPublished};
     collection.push (album);
     return console.log(album);
 }   //Add a function named addToCollection. This function should:
@@ -23,3 +23,13 @@ console.log(collection); //Test the addToCollection function:
     //Add 6 albums to your collection. Aim to have a mix of both same and different artists and published years. (Feel free to share your musical interests, or make stuff up. Totally fine either way.)
     //Console.log each album as added using the returned value.
     //After all are added, console.log the collection array.
+
+function showCollection (array) {
+    console.log(array.length);
+    for (let index = 0; index <= array.length; index++) {
+        console.log(array[index]);
+    }
+    
+}
+
+console.log(showCollection(collection));
